08bbcb405fbddb2eb7b4242fb76b015ee0b9b582
Inivak2023 <149315444+Inivak2023@users.noreply.github.com>
2024-11-30 14:08:09 +0530

    Add files via upload

---
+++ video.js
@@ -0,0 +1,14 @@
+// video.js
+const videoSrc = "https://drive.google.com/file/d/1arq2HqsdgJJzJRUMbWXWHxg-u8cCHq1n/preview"; // Google Drive වීඩියෝ ලින්ක්
+
+function getVideoSrc() {
+  return videoSrc; // Dynamic link returned here
+}
+
+
+// video.js
+const videoSrc1 = "https://drive.google.com/file/d/1arq2HqsdgJJzJRUMbWXWHxg-u8cCHq1n/preview"; // Google Drive වීඩියෝ ලින්ක්
+
+function getVideoSrc1() {
+  return videoSrc1; // Dynamic link returned here
+}
